1521bed0fc6be4296fe60434f59c274c7a4b45dd
JB van der Merwe <jb@jbkamp.com>
2022-04-25 17:59:31 +0200

    Continued update to calculator key

--- lib/ui/widgets/buttons/calculatorkeybutton/calculatorkeybutton_view.dart
+++ lib/ui/widgets/buttons/calculatorkeybutton/calculatorkeybutton_view.dart
@@ -1,25 +1,45 @@
-import 'package:calculator/enums/operationtype_enum.dart';
+import 'package:calculator/enums/nondigitoperation_enum.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
 import 'calculatorkeybutton_viewmodel.dart';
 
 class CalculatorKeyButtonView extends StatelessWidget {
-  CalculatorKeyButtonView({Key? key, required this.digit}) : super(key: key);
+  CalculatorKeyButtonView({Key? key, this.digit, this.nonDigitOperation}) : super(key: key);
 
-  int digit;
+  final int?                digit;
+  final NonDigitOperation?  nonDigitOperation;
+
 
   @override
   Widget build(BuildContext context) {
+    if(digit == null && nonDigitOperation == null) {
+      throw Exception("Digit & Non-Digit operation can't both be null");
+    } else if (digit != null && nonDigitOperation != null) {
+      throw Exception("Digit & Non-Digit operation can't both be not null.  Select only one");
+    }
     return ViewModelBuilder<CalculatorKeyButtonViewModel>.reactive(
       builder: (BuildContext context, CalculatorKeyButtonViewModel model, Widget? child) {
         return InkWell(
-          onTap: (){},
+          onTap: model.onButtonTapped,
           child: Container(
-            child: Text("$digit", style: TextStyle(fontSize: 25),),
+            alignment: Alignment.center,
+            child: Text("${digit ?? nonDigitOperation!.symbol}", style: TextStyle(fontSize: 25),),
             decoration: BoxDecoration(
               color: Colors.grey.shade200,
-              borderRadius: BorderRadius.all(Radius.circular(8),)
+              borderRadius: BorderRadius.all(Radius.circular(model.boderRadius),)
+            ),
+            margin: EdgeInsets.only(
+              left:   model.margin, 
+              right:  model.margin, 
+              top:    model.margin, 
+              bottom: model.margin
+            ),
+            padding: EdgeInsets.only(
+              left:   model.padding,
+              right:  model.padding,
+              top:    model.padding,
+              bottom: model.padding
             ),
           )
         );
--- lib/ui/widgets/buttons/calculatorkeybutton/calculatorkeybutton_viewmodel.dart
+++ lib/ui/widgets/buttons/calculatorkeybutton/calculatorkeybutton_viewmodel.dart
@@ -1,8 +1,19 @@
 import 'package:calculator/app/locator.dart';
 import 'package:calculator/enums/operationtype_enum.dart';
 import 'package:calculator/services/calculationservice.dart';
+import 'package:get/get.dart';
 import 'package:stacked/stacked.dart';
 
 class CalculatorKeyButtonViewModel extends BaseViewModel {
 
+  double _buttonWidth = Get.width/3;
+
+  double  get margin      => 4;       
+  double  get padding     => 16;       
+  double  get buttonWidth => _buttonWidth;
+  double  get boderRadius => 8;
+
+  void onButtonTapped() {
+
+  }
 }
