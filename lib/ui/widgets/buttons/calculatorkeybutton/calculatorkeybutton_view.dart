c5fdd2865affede4edcaf7d5a688971954e8d32f
JB van der Merwe <jb@jbkamp.com>
2022-04-25 17:10:46 +0200

    Initial Digit Key design.

--- lib/ui/widgets/buttons/calculatorkeybutton/calculatorkeybutton_view.dart
+++ lib/ui/widgets/buttons/calculatorkeybutton/calculatorkeybutton_view.dart
@@ -5,16 +5,24 @@ import 'package:stacked/stacked.dart';
 import 'calculatorkeybutton_viewmodel.dart';
 
 class CalculatorKeyButtonView extends StatelessWidget {
-  CalculatorKeyButtonView({Key? key}) : super(key: key);
+  CalculatorKeyButtonView({Key? key, required this.digit}) : super(key: key);
+
+  int digit;
 
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<CalculatorKeyButtonViewModel>.reactive(
       builder: (BuildContext context, CalculatorKeyButtonViewModel model, Widget? child) {
-        return IconButton(
-          onPressed: (){}, 
-          icon: Icon(Icons.place)
-          );
+        return InkWell(
+          onTap: (){},
+          child: Container(
+            child: Text("$digit", style: TextStyle(fontSize: 25),),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: BorderRadius.all(Radius.circular(8),)
+            ),
+          )
+        );
       },
       viewModelBuilder: () => CalculatorKeyButtonViewModel(),
     );
