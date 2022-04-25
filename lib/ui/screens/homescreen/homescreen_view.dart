4d5e0ebfb3a503349bc7a31a8fd8276b3e80c4f1
JB van der Merwe <jb@jbkamp.com>
2022-04-25 12:37:14 +0200

    Dynamic operation type button builder

--- lib/ui/screens/homescreen/homescreen_view.dart
+++ lib/ui/screens/homescreen/homescreen_view.dart
@@ -1,3 +1,5 @@
+import 'package:calculator/enums/operationtype_enum.dart';
+import 'package:calculator/ui/widgets/buttons/calculatorkeybutton/calculatorkeybutton_view.dart';
 import 'package:calculator/ui/widgets/buttons/operationbutton/operationbutton_view.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
@@ -12,16 +14,12 @@ class HomeScreenView extends StatelessWidget {
     return ViewModelBuilder<HomeScreenViewModel>.reactive(
       builder: (BuildContext context, HomeScreenViewModel model, Widget? child) {
         return Scaffold(
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              OperationButton(onPressed: model.plusTapped, title: "Plus"),
-              OperationButton(onPressed: model.minusTapped, title: "Minus"),
-              OperationButton(onPressed: model.multiplyTapped, title: "Multiply"),
-              OperationButton(onPressed: model.divideTapped, title: "Divide"),
-              OperationButton(onPressed: model.sqrtTapped, title: "Square Route"),
-            ],
+          body: ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              
+              return OperationButton(operationType: OperationType.values[index],);
+            },
+            itemCount: OperationType.values.length,
           ),
         );
       },
