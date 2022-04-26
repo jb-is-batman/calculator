4b3fb51ff7c83c637bab65148b2be1f7b6549119
JB van der Merwe <jb@jbkamp.com>
2022-04-26 09:35:41 +0200

    Set which operand is active.

--- lib/ui/widgets/buttons/calculatoractionbutton/calculatoractionbutton_viewmodel.dart
+++ lib/ui/widgets/buttons/calculatoractionbutton/calculatoractionbutton_viewmodel.dart
@@ -1,3 +1,19 @@
+import 'package:calculator/app/locator.dart';
+import 'package:calculator/enums/calculatoraction_enum.dart';
+import 'package:calculator/services/calculationservice.dart';
 import 'package:stacked/stacked.dart';
 
-class CalculatorActionButtonViewModel extends BaseViewModel {}
+class CalculatorActionButtonViewModel extends BaseViewModel {
+  
+  final CalculationService    _calculationService     = getIt.get<CalculationService>();
+
+  void executeAction(CalculatorAction value) {
+    switch (value) {
+      case CalculatorAction.backspace:
+        
+        _calculationService.backspaceOperand1();
+        break;
+      default:
+    }
+  }
+}
