da0e58d9ab4d1cd1696c414ed4985cc0810d044d
JB van der Merwe <jb@jbkamp.com>
2022-04-27 11:22:36 +0200

    Updated default colors.

--- lib/app/defaultui.dart
+++ lib/app/defaultui.dart
@@ -5,8 +5,9 @@ const double        kaDefaultMultiple     = 8.0;
 
 // UI components constants
 const BorderRadius  kaDefaultBorderRadius = BorderRadius.all(Radius.circular(4),);
-const Color         kaDefaultButtonColor  = Colors.blueAccent;
-
 // Default margin & padding
 const double        kaDefaultMargin       = 8;
 const double        kaDefaulPadding       = 16;
+
+const Color         kaDefaultErrorColor   = Color.fromRGBO(238, 155, 0, 1);
+const Color         kaDefaultButtonColor  = Color.fromRGBO(173, 232, 244, 1);
