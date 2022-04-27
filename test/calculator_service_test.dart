f13e4f9df18b69b5a1ac8be9aa0127ebabf0fd1d
JB van der Merwe <jb@jbkamp.com>
2022-04-27 13:27:31 +0200

    Refactored calculator service test.

--- test/calculator_service_test.dart
+++ test/calculator_service_test.dart
@@ -1,27 +1,29 @@
+import 'package:calculator/app/locator.dart';
 import 'package:calculator/enums/operationtype_enum.dart';
 import 'package:calculator/services/calculationservice.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
-class MockCalculatorService extends Mock implements CalculationService {}
+class MockCalculatorService             extends Mock implements CalculationService {}
 
 void main() {
-  CalculationService systemUnderTest = CalculationService();
+  setupLocator();
+  CalculationService sutCalculationService = CalculationService();
 
   test("Initial values are correct", (){
-    expect(systemUnderTest.screenIndex, 0);
-    expect(systemUnderTest.operationType, OperationType.none);
-    expect(systemUnderTest.operand, "");
-    expect(systemUnderTest.title, "Select Operand 2");
-    expect(systemUnderTest.isNextEnabled, false);
+    expect(sutCalculationService.screenIndex, 0);
+    expect(sutCalculationService.operationType, OperationType.none);
+    expect(sutCalculationService.operand, "");
+    expect(sutCalculationService.title, "Select Operand 2");
+    expect(sutCalculationService.isNextEnabled, false);
   });
 
   group("Page index testing", () {
     test("Article index change", (){
-      systemUnderTest.setScreenIndex(1);
-      expect(systemUnderTest.screenIndex, 1);
-      systemUnderTest.setScreenIndex(5);
-      expect(systemUnderTest.screenIndex, 1);
+      sutCalculationService.setScreenIndex(1);
+      expect(sutCalculationService.screenIndex, 1);
+      sutCalculationService.setScreenIndex(5);
+      expect(sutCalculationService.screenIndex, 1);
     });
   });
 }
